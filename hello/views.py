3c07c6a03131692b76496ce086859ae48b78d7db
mdacht777 <mdacht777@gmail.com>
2021-08-05 14:46:38 -0600

    views.py

--- hello/views.py
+++ hello/views.py
@@ -1,29 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+# 
+from flask import Flask
 
-from .models import Greeting
-import requests
-import os
+app = Flask(__name__)
 
-# Create your views here.
-def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
-
-# def index(request):
-#     r = requests.get('http://httpbin.org/status/418')
-#     print(r.text)
-#     return HttpResponse('<pre>' + r.text + '</pre>')
-
-# def index(request):
-#     times = int(os.environ.get('TIMES',3))
-#     return HttpResponse('Hello! ' * times)
-
-def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, "db.html", {"greetings": greetings})
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
