2e89c013968c30b60945c92e63702c3c55be525b
mdacht777 <mdacht777@gmail.com>
2021-08-05 15:08:46 -0600

    reverting

--- hello/views.py
+++ hello/views.py
@@ -1,8 +1,29 @@
-# 
-from flask import Flask
+from django.shortcuts import render
+from django.http import HttpResponse
 
-app = Flask(__name__)
+from .models import Greeting
+import requests
+import os
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# Create your views here.
+def index(request):
+    # return HttpResponse('Hello from Python!')
+    return render(request, "index.html")
+
+# def index(request):
+#     r = requests.get('http://httpbin.org/status/418')
+#     print(r.text)
+#     return HttpResponse('<pre>' + r.text + '</pre>')
+
+# def index(request):
+#     times = int(os.environ.get('TIMES',3))
+#     return HttpResponse('Hello! ' * times)
+
+def db(request):
+
+    greeting = Greeting()
+    greeting.save()
+
+    greetings = Greeting.objects.all()
+
+    return render(request, "db.html", {"greetings": greetings})
