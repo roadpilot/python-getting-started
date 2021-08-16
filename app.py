33f09e8dfb0f7f20b2094cbaaf8af70fec7cdeac
mdacht777 <mdacht777@gmail.com>
2021-08-16 11:46:24 -0600

    push changes

--- app.py
+++ app.py
@@ -6,17 +6,24 @@
 # def hello_world():
 #     return "<p>Hello, World!</p>"
 
-from flask import Flask, send_from_directory
-from flask_restful import Api, Resource, reqparse
+from flask import Flask
+# from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS #comment this on deployment
-from api.HelloApiHandler import HelloApiHandler
+# from api.HelloApiHandler import HelloApiHandler
 
-app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+app = Flask(__name__)
 CORS(app) #comment this on deployment
-api = Api(app)
+# api = Api(app)
 
-@app.route("/", defaults={'path':''})
-def serve(path):
-    return send_from_directory(app.static_folder,'index.html')
+# @app.route("/", defaults={'path':''})
+# def serve(path):
+#     return send_from_directory(app.static_folder,'index.html')
 
-api.add_resource(HelloApiHandler, '/flask/hello')
+# api.add_resource(HelloApiHandler, '/flask/hello')
+
+@app.route("/")
+def hello_world():
+    return {
+      'resultStatus': 'SUCCESS',
+      'message': "Hello, World!"
+      }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -34,7 +34,7 @@ function App() {
   const [getMessage, setGetMessage] = useState({})
 
   useEffect(()=>{
-    axios.get('http://localhost:5000/flask/hello').then(response => {
+    axios.get('http://localhost:5000').then(response => {
       console.log("SUCCESS", response)
       setGetMessage(response)
     }).catch(error => {
@@ -48,10 +48,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>React + Flask Tutorial</p>
-        <div>{getMessage.status === 200 ? 
+        {/* <div>{getMessage.status === 200 ? 
           <h3>{getMessage.data.message}</h3>
           :
-          <h3>LOADING</h3>}</div>
+          <h3>LOADING</h3>}</div> */}
+        <div>{
+          getMessage.status === 200 
+          ? 
+          <h3>{getMessage.data.message}</h3>
+          :
+          <h3>LOADING</h3>
+          }
+       </div>
       </header>
     </div>
   );
